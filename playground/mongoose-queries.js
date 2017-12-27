2737d75ce692410acc70d8445fedfc3ebd11ad04
Raheekhan <ibrahimkhan0894@gmail.com>
2017-12-27 13:20:56 -0500

    Added GET /todos/:id and test cases [#5]

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,43 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5a43263c691a6125fd6319e1';
+
+// var id = '5a43caf90d9e5229d412e42b';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
