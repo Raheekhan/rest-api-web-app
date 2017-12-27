ffa7ee132e3ebda1e5ca0e32e9138b560139a4c6
Raheekhan <ibrahimkhan0894@gmail.com>
2017-12-27 11:14:29 -0500

    Added GET /todos route [#4]

--- server/server.js
+++ server/server.js
@@ -21,8 +21,12 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.post('/users', (req, res) => {
-  console.log(req.body);
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
